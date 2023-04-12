1950cc032d4d8b428b42e4bce4457749a8a2a594
Harsh Singh <sujeetsingh734321@gmail.com>
2023-04-12 15:22:06 +0530

    chnages

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-
+// Set up a route for the home page
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+// Set up a connection event for Socket.io
+io.on('connection', (socket) => {
+  console.log('A user connected.');
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+  // Join a room
+  socket.on('join', (room) => {
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+  });
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
-})
+  // Leave a room
+  socket.on('leave', (room) => {
+    socket.leave(room);
+    console.log(`User left room: ${room}`);
+  });
+
+  // Set up a custom event for receiving messages
+  socket.on('chat message', (room, msg) => {
+    console.log(`Message in room ${room}: ${msg}`);
+
+    // Broadcast the message to all clients in the room
+    io.to(room).emit('chat message', msg);
+  });
+
+  // Set up a disconnect event
+  socket.on('disconnect', () => {
+    console.log('A user disconnected.');
+  });
+});
 
 // Start the server
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Server started on port 3000');
+http.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
- 
